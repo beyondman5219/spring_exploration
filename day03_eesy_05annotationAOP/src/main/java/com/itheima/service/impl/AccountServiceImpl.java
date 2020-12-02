4a73eb087ce102d050c3848b6338bb4ae2e88f3a
beond_man <beyond_man5219@163.com>
2020-12-02 21:47:06 +0800

    切当表达式的注解方式，自定义注解

--- day03_eesy_05annotationAOP/src/main/java/com/itheima/service/impl/AccountServiceImpl.java
+++ day03_eesy_05annotationAOP/src/main/java/com/itheima/service/impl/AccountServiceImpl.java
@@ -1,5 +1,6 @@
 package com.itheima.service.impl;
 
+import com.itheima.annotation.Monitor;
 import com.itheima.service.IAccountService;
 import org.springframework.stereotype.Service;
 
@@ -7,21 +8,24 @@ import org.springframework.stereotype.Service;
  * 账户的业务层实现类
  */
 @Service("accountService")
-public class AccountServiceImpl implements IAccountService{
+public class AccountServiceImpl implements IAccountService {
 
     @Override
+    @Monitor
     public void saveAccount() {
         System.out.println("执行了保存");
-        int i=1/0;
+//        int i=1/0;
     }
 
     @Override
+    @Monitor
     public void updateAccount(int i) {
-        System.out.println("执行了更新"+i);
+        System.out.println("执行了更新" + i);
 
     }
 
     @Override
+    @Monitor
     public int deleteAccount() {
         System.out.println("执行了删除");
         return 0;
