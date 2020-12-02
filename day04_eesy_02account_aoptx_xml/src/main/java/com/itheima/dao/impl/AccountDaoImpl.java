36e787a937ade516d933692913db375492cea9e6
beond_man <beyond_man5219@163.com>
2020-12-02 19:30:44 +0800

    更名

--- day04_eesy_02account_aoptx_xml/src/main/java/com/itheima/dao/impl/AccountDaoImpl.java
+++ day04_eesy_02account_aoptx_xml/src/main/java/com/itheima/dao/impl/AccountDaoImpl.java
@@ -17,6 +17,7 @@ public class AccountDaoImpl implements IAccountDao {
     private QueryRunner runner;
 
     private ConnectionUtils connectionUtils;
+
     public void setRunner(QueryRunner runner) {
         this.runner = runner;
     }
@@ -26,63 +27,63 @@ public class AccountDaoImpl implements IAccountDao {
     }
 
 
-
     public List<Account> findAllAccount() {
-        try{
-            return runner.query(connectionUtils.getThreadConnection(),"select * from account",new BeanListHandler<Account>(Account.class));
-        }catch (Exception e) {
+        try {
+            return runner.query(connectionUtils.getThreadConnection(), "select * from account", new BeanListHandler<Account>(Account.class));
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
     @Override
     public Account findAccountById(Integer accountId) {
-        try{
-            return runner.query(connectionUtils.getThreadConnection(),"select * from account where id = ? ",new BeanHandler<Account>(Account.class),accountId);
-        }catch (Exception e) {
+        try {
+            return runner.query(connectionUtils.getThreadConnection(), "select * from account where id = ? ", new BeanHandler<Account>(Account.class), accountId);
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
     @Override
     public void saveAccount(Account account) {
-        try{
-            runner.update(connectionUtils.getThreadConnection(),"insert into account(name,money)values(?,?)",account.getName(),account.getMoney());
-        }catch (Exception e) {
+        try {
+            runner.update(connectionUtils.getThreadConnection(), "insert into account(name,money)values(?,?)", account.getName(), account.getMoney());
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
     @Override
     public void updateAccount(Account account) {
-        try{
-            runner.update(connectionUtils.getThreadConnection(),"update account set name=?,money=? where id=?",account.getName(),account.getMoney(),account.getId());
-        }catch (Exception e) {
+        try {
+//            int i = 1 / 0;
+            runner.update(connectionUtils.getThreadConnection(), "update account set name=?,money=? where id=?", account.getName(), account.getMoney(), account.getId());
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
     @Override
     public void deleteAccount(Integer accountId) {
-        try{
-            runner.update(connectionUtils.getThreadConnection(),"delete from account where id=?",accountId);
-        }catch (Exception e) {
+        try {
+            runner.update(connectionUtils.getThreadConnection(), "delete from account where id=?", accountId);
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
     @Override
     public Account findAccountByName(String accountName) {
-        try{
-            List<Account> accounts = runner.query(connectionUtils.getThreadConnection(),"select * from account where name = ? ",new BeanListHandler<Account>(Account.class),accountName);
-            if(accounts == null || accounts.size() == 0){
+        try {
+            List<Account> accounts = runner.query(connectionUtils.getThreadConnection(), "select * from account where name = ? ", new BeanListHandler<Account>(Account.class), accountName);
+            if (accounts == null || accounts.size() == 0) {
                 return null;
             }
-            if(accounts.size() > 1){
+            if (accounts.size() > 1) {
                 throw new RuntimeException("结果集不唯一，数据有问题");
             }
             return accounts.get(0);
-        }catch (Exception e) {
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
